04ae38b2fd89f75cdb72782fb92761f41c8f4c51
Thomas Vollstädt <thomas.vollstaedt.extern@ista.com>
2020-12-05 01:32:41 +0100

    :sparkles: Solved day #3
    Signed-off-by: Thomas Vollstädt <thomas.vollstaedt.extern@ista.com>

--- _3.py
+++ _3.py
@@ -1,11 +1,23 @@
-from typing import List
+from typing import List, Tuple
 
 
-def part1(input: List[str]):
-    counter = 0
-    return counter
+def part1(input: List[str], slope_x, slope_y):
+    counter, x, y = [0, 0, 0]
+    geo_map = [[ch for ch in list(line)] for line in input]
+    while True:
+        x += slope_x
+        y += slope_y
+
+        if y > len(geo_map) - 1:
+            return counter
+        if x > len(geo_map[y]) - 1:
+            x -= len(geo_map[y])
+        if geo_map[y][x] == "#":
+            counter += 1
 
 
-def part2(input: List[str]):
-    counter = 0
-    return counter
+def part2(input: List[str], slopes: List[Tuple[int, int]]):
+    amount = 1
+    for slope_x, slope_y in slopes:
+        amount *= part1(input, slope_x, slope_y)
+    return amount
--- test/3/test.py
+++ test/3/test.py
@@ -14,12 +14,13 @@ def readfile(filename):
 class SolverTest(unittest.TestCase):
 
     def test_part1(self):
-        self.assertEqual(1, solver.part1(readfile('test_input')))
-        print("Day 3 Part 1 solution: ", solver.part1(readfile('real_input')))
+        self.assertEqual(7, solver.part1(readfile('test_input'), 3, 1))
+        print("Day 3 Part 1 solution: ", solver.part1(readfile('real_input'), 3, 1))
 
     def test_part2(self):
-        self.assertEqual(1, solver.part2(readfile('test_input')))
-        print("Day 3 Part 2 solution: ", solver.part2(readfile('real_input')))
+        try_slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        self.assertEqual(336, solver.part2(readfile('test_input'), try_slopes))
+        print("Day 3 Part 2 solution: ", solver.part2(readfile('real_input'), try_slopes))
 
 
 if __name__ == '__main__':
